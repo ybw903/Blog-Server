94b0f799de44f109e5129e0db9632147056589a1
유병우 <conner903@naver.com>
2022-06-08 20:35:01 +0900

    feat: fetch post

---
+++ src/data/posts.ts
@@ -0,0 +1,6 @@
+const posts = [
+  { id: 1, title: "test1", content: "test1" },
+  { id: 2, title: "test2", content: "test2" },
+];
+
+export default posts;
--- src/schema.ts
+++ src/schema.ts
@@ -1,10 +1,18 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { gql } from "apollo-server-express";
 import { GraphQLSchema } from "graphql";
+import posts from "./data/posts";
 
 const typeDef = gql`
   type Query {
     helloWorld: String!
+    getPosts: [Post!]!
+  }
+
+  type Post {
+    id: ID!
+    title: String
+    content: String
   }
 `;
 
@@ -12,6 +20,9 @@ const resolverMap = {
   Query: {
     helloWorld(_: void, args: void): string {
       return `Hello GraphQL~!`;
+    },
+    getPosts() {
+      return posts;
     },
   },
 };
